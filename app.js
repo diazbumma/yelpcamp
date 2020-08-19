350e50ba936df585843f1477156f055dc1dfe8f8
Odiaz Bumma <odiazbumma@gmail.com>
2020-08-19 21:06:15 +0700

    Add view campgrounds page

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ app.get('/', function(req, res) {
     res.render('home')
 })
 
+app.get('/campgrounds', function(req, res) {
+    let campgrounds = [
+        {name: 'Merapi Park', image: 'https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350'},
+        {name: 'Tidar Camp', image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350'},
+        {name: 'Mountain Camp', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350'},
+        {name: 'Kaliurang Camp', image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350'}
+    ]
+
+    res.render('campgrounds', {campgrounds: campgrounds})
+})
+
+app.post('/campgrounds', function(req, res) {
+    
+})
+
 app.get('*', function(req, res) {
     res.send('Ups, I think you lost buddy..')
 })
