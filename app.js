2183da06c4cf903413ce4e9be9776d1888608297
Odiaz Bumma <odiazbumma@gmail.com>
2020-08-19 21:50:38 +0700

    Add page to add new campground

--- app.js
+++ app.js
@@ -6,23 +6,30 @@ const PORT = 3000
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
+let campgrounds = [
+    {name: 'Merapi Park', image: 'https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350'},
+    {name: 'Tidar Camp', image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350'},
+    {name: 'Mountain Camp', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350'},
+    {name: 'Kaliurang Camp', image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350'}
+]
+
 app.get('/', function(req, res) {
     res.render('home')
 })
 
 app.get('/campgrounds', function(req, res) {
-    let campgrounds = [
-        {name: 'Merapi Park', image: 'https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350'},
-        {name: 'Tidar Camp', image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350'},
-        {name: 'Mountain Camp', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350'},
-        {name: 'Kaliurang Camp', image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350'}
-    ]
-
     res.render('campgrounds', {campgrounds: campgrounds})
 })
 
 app.post('/campgrounds', function(req, res) {
-    
+    let name = req.body.name
+    let image = req.body.image
+    campgrounds.push({name: name, image: image})
+    res.redirect('/campgrounds')
+})
+
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new')
 })
 
 app.get('*', function(req, res) {
