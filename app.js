ba5aada680421bdeef6f3aabdaf8baef02ae7872
Odiaz Bumma <odiazbumma@gmail.com>
2020-09-09 19:08:20 +0700

    Add comments route

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', {
 }).then(() => console.log('Connected to yelp_camp DB!')).catch(error => console.log(error.message))
 
 const Campground = require('./models/campground')
+const Comment = require('./models/comment')
 const seed = require('./seeds')
 
 seed()
@@ -57,13 +58,41 @@ app.get('/campgrounds/new', function(req, res) {
 })
 
 app.get('/campgrounds/:id', function(req, res) {
-    Campground.findById({_id: req.params.id}).populate('comments').exec(function(err, data) {
+    Campground.findById(req.params.id).populate('comments').exec(function(err, data) {
         if (err) {
             console.log('Something wrong while retrieving data')
         } else {
             console.log('Retrieving detailed campground info successful')
             console.log(data)
             res.render('campgrounds/camp_info', {campground: data})
+        }
+    })
+})
+
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+    res.render('comments/new', {campgroundId: req.params.id})
+})
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err)
+        } else {
+            Comment.create(req.body.comment, function(err, createdComment) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    foundCampground.comments.push(createdComment)
+                    foundCampground.save(function(err, pushedItem) {
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            console.log('comment created')
+                            res.redirect('/campgrounds/' + req.params.id)
+                        }
+                    })
+                }
+            })
         }
     })
 })
