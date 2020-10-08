0dd048b07313de06a1a1bb3675ecfc06db04bf68
diazbumma <odiazbumma@gmail.com>
2020-10-08 16:38:37 +0700

    Refactoring landing page, add image slider background

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(flash())
+app.use(express.static(__dirname + "/public"))
 mongoose.connect('mongodb://localhost/yelp_camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
